eb6b5a5d26058b01be440b583289c562b656c7cd
andrew.mintah <andrew.mintah@bigspark.dev>
2025-11-15 17:52:30 +0000

    init

---
+++ src/test/java/com/app/whiteboard/WhiteboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.whiteboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhiteboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
